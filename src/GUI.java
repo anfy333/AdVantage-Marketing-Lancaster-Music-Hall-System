130aac6862823cf75932af8f746a1bfd3d5d21ac
anfy333 <anfisab@birs.ru>
2025-03-23 21:16:41 +0000

    connected a javafx sdk library and created a gui class

---
+++ src/GUI.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends Application {
+    /*JFrame frame;
+    JPanel panel;
+    //JPanel panel = new JPanel();
+    public GUI() {
+        frame = new JFrame();
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+        frame.setSize(800, 800);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }*/
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        new GUI(); // Create an instance of GUI to invoke the constructor
+    }
+}
